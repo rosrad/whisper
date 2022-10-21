4b10f56b310f74b1ba7fb43517fe6f08313bc5b2
Bo Ren <boren@microsoft.com>
2022-10-20 20:36:48 -0700

    add notebook and configs

---
+++ notebook.py
@@ -0,0 +1,36 @@
+# %%
+import torch
+import whisper
+
+# %%
+model_names = [
+    'tiny.en',
+    'tiny',
+    'base.en',
+    'base',
+    'small.en',
+    'small',
+    'medium.en',
+    'medium',
+    'large']
+model_name = "base"
+
+audio_file = "/home/boren/data/LibriSpeech/test-clean/1089/134686/1089-134686-0000.flac"
+model = whisper.load_model(model_name, download_root="data/models")
+# model_file = f"/home/boren/whisper/data/models/{model_name}.pt"
+# state_dict = torch.load(model_file, map_location="cpu")
+# %%
+result = model.transcribe(audio_file)
+print(result["text"])
+
+x = torch.rand(1, 80, 3000).to(model.device)
+
+torch.onnx.export(
+    model.encoder,
+    x,
+    "base.dec.onnx",
+    export_params=True,
+    do_constant_folding=True,
+    verbose=True,
+    opset_version=12,
+)
